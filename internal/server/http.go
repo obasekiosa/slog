b5134a37e21ad39e347d97ae442c0c1d36e3c3bb
Osakpolor Obaseki <obasekiosa@gmail.com>
2021-10-13 13:08:00 +0100

    Fix: typos

--- internal/server/http.go
+++ internal/server/http.go
@@ -23,7 +23,7 @@ type httpServer struct {
 
 func newHTTPServer() *httpServer {
     return &httpServer{
-        Log: NewLog()
+        Log: NewLog(),
     }
 }
 type ProduceRequest struct {
@@ -40,4 +40,49 @@ type ConsumeRequest struct {
 
 type ConsumeResponse struct {
     Record Record `json:"record"`
+}
+
+func (s *httpServer) handleProduce(w http.ResponseWriter, r *http.Request) {
+    var req ProduceRequest
+    err := json.NewDecoder(r.Body).Decode(&req)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+    off, err := s.Log.Append(req.Record)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    res := ProduceResponse{Offset: off}
+    err = json.NewEncoder(w).Encode(res)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+}
+
+func (s *httpServer) handleConsume(w http.ResponseWriter, r *http.Request) {
+    var req ConsumeRequest
+    err := json.NewDecoder(r.Body).Decode(&req)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+    record, err := s.Log.Read(req.Offset)
+    if err == ErrOffsetNotFound {
+        http.Error(w, err.Error(), http.StatusNotFound)
+        return
+    }
+
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusNotFound)
+        return
+    }
+    res := ConsumeResponse{Record: record}
+    err = json.NewEncoder(w).Encode(res)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 }
